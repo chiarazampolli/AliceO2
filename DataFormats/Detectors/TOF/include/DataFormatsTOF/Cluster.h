487abb67ce93253cb94b1ecd602e60e5c70c6273
zampolli <Chiara.Zampolli@cern.ch>
2018-06-13 17:36:25 +0200

    Going on with clusterization - but still not finished nor compiling

--- DataFormats/Detectors/TOF/include/DataFormatsTOF/Cluster.h
+++ DataFormats/Detectors/TOF/include/DataFormatsTOF/Cluster.h
@@ -29,6 +29,16 @@ namespace TOF
 class Cluster : public o2::BaseCluster<float>
 {
  public:
+  enum {kMain      = 0x3FFFF,
+	kUpLeft    = 0x40000,
+	kUp        = 0x80000,
+	kUpRight   = 0x100000,
+	kRight     = 0x200000,
+	kDownRight = 0x400000,
+	kDown      = 0x800000,
+	kDownLeft  = 0x1000000,
+	kLeft      = 0x2000000}
+
   Cluster() = default;
 
   Cluster(std::int16_t sensid, float x, float y, float z, float sy2, float sz2, float syz, float timeRaw, float time, float tot, int L0L1latency, int deltaBC);
@@ -50,12 +60,42 @@ class Cluster : public o2::BaseCluster<float>
   float  getPhi() const   {return mPhi;} // Cluster Phi
 
   int    getContributingChannels() const {return mContributingChannels;}
-  int    getNumOfContributingChannels() const; // returns the number of hits associated to the cluster, i.e. the number of hits that built the cluster; it is the equivalente of teh old AliESDTOFCluster::GetNTOFhits() 
-  int    getMainContributingChannel() const {return mContributingChannels & 0x3FFFF;}
-  bool   isUpContributing() const {return (mContributingChannels & 0x40000) == 0x40000 ? 1 : 0;}
-  bool   isDownContributing() const {return (mContributingChannels & 0x80000) == 0x80000 ? 1 : 0;}
-  bool   isLeftContributing() const {return (mContributingChannels & 0x100000) == 0x100000 ? 1 : 0;}
-  bool   isRightContributing() const {return (mContributingChannels & 0x200000) == 0x200000 ? 1 : 0;}
+  void   setContributingChannels(int contributingChannels) {mContributingChannels = contributingChannels;}
+  void   addBitInContributingChannels(int mask) {mContributingChannels |= mask;}
+  int    getNumOfContributingChannels() const; // returns the number of hits associated to the cluster, i.e. the number of hits that built the cluster; it is the equivalente of the old AliESDTOFCluster::GetNTOFhits() 
+  int    getMainContributingChannel() const {return mContributingChannels & kMain;}
+
+  void   setMainContributingChannel(int newvalue) {int mask = kMain; mContributingChannels &= ~mask; mContributingChannel |= newvalue & mask;} // first we do "bitwise-and" with all bits set to 1 but the first 18, which is the negation of "mask"
+
+  // setting the up, down, right, left bits
+  void   setUpLeftContributingChannel()    {mContributingChannel |= kUpLeft;}    // we do "bitwise-or" with the 19th bit
+  void   setUpContributingChannel()        {mContributingChannel |= kUp;}        // we do "bitwise-or" with the 20th bit
+  void   setUpRightContributingChannel()   {mContributingChannel |= kUpRight;}   // we do "bitwise-or" with the 21th bit
+  void   setRightContributingChannel()     {mContributingChannel |= kRight;}     // we do "bitwise-or" with the 22th bit
+  void   setDownRightContributingChannel() {mContributingChannel |= kDownRight;} // we do "bitwise-or" with the 23th bit
+  void   setDownContributingChannel()      {mContributingChannel |= kDown;}      // we do "bitwise-or" with the 24th bit
+  void   setDownLeftContributingChannel()  {mContributingChannel |= kDownLeft;}  // we do "bitwise-or" with the 25th bit
+  void   setLeftContributingChannel()      {mContributingChannel |= kLeft;}      // we do "bitwise-or" with the 26th bit
+
+  // resetting the up, down, right, left bits
+  void   resetUpLeftContributingChannel()    {mContributingChannels &= ~kUpLeft;}    // we do "bitwise-and" with the 19th bit only set to zero, which is the negation of "mask"
+  void   resetUpContributingChannel()        {mContributingChannels &= ~kUp;}        // we do "bitwise-and" with the 20th bit only set to zero, which is the negation of "mask"
+  void   resetUpRightContributingChannel()   {mContributingChannels &= ~kUpRight;}   // we do "bitwise-and" with the 21th bit only set to zero, which is the negation of "mask"
+  void   resetRightContributingChannel()     {mContributingChannels &= ~kRight;}     // we do "bitwise-and" with the 22th bit only set to zero, which is the negation of "mask"
+  void   resetDownRightContributingChannel() {mContributingChannels &= ~kDownRight;} // we do "bitwise-and" with the 23th bit only set to zero, which is the negation of "mask"
+  void   resetDownContributingChannel()      {mContributingChannels &= ~kDown;}      // we do "bitwise-and" with the 24th bit only set to zero, which is the negation of "mask"
+  void   resetDownLeftContributingChannel()  {mContributingChannels &= ~kDownLeft;}  // we do "bitwise-and" with the 25th bit only set to zero, which is the negation of "mask"
+  void   resetLeftContributingChannel()      {mContributingChannels &= ~kLeft;}      // we do "bitwise-and" with the 26th bit only set to zero, which is the negation of "mask"
+
+  // getters of the up, down, right, left bits
+  bool   isUpLeftContributing()    const {return (mContributingChannels & kUpLeft) == kUpLeft ? 1 : 0;}
+  bool   isUpContributing()        const {return (mContributingChannels & kUp) == kUp ? 1 : 0;}
+  bool   isUpRightContributing()   const {return (mContributingChannels & kUpRight) == kUpRight ? 1 : 0;}
+  bool   isRightContributing()     const {return (mContributingChannels & kRight) == kRight ? 1 : 0;}
+  bool   isDownRightContributing() const {return (mContributingChannels & kDownRight) == kDownRight ? 1 : 0;}
+  bool   isDownContributing()      const {return (mContributingChannels & kDown) == kDown ? 1 : 0;}
+  bool   isDownLeftContributing()  const {return (mContributingChannels & kDownLeft) == kDownLeft ? 1 : 0;}
+  bool   isLeftContributing()      const {return (mContributingChannels & kLeft) == kLeft ? 1 : 0;}
   
  private:
   friend class boost::serialization::access;
@@ -69,11 +109,15 @@ class Cluster : public o2::BaseCluster<float>
   float  mR;           //! radius
   float  mPhi;         //! phi coordinate 
   int    mContributingChannels;       // index of the channels that contributed to the cluster; to be read like this:
-                                      //channel & 0x3FFFF -> first 18 bits to strore the main channel
-                                      //channel & bit19 (0x40000) -> alsoUP
-                                      //channel & bit20 (0x80000)-> alsoDOWN
-                                      //channel & bit21 (0x100000)-> alsoRIGHT
-                                      //channel & bit22 (0x200000)-> alsoLEFT
+                                      // channel & 0x3FFFF -> first 18 bits to store the main channel
+                                      // channel & bit19 (0x40000) -> alsoUPLEFT
+                                      // channel & bit20 (0x80000) -> alsoUP
+                                      // channel & bit21 (0x100000)-> alsoUPRIGHT
+                                      // channel & bit22 (0x200000)-> alsoRIGHT
+                                      // channel & bit23 (0x400000)-> alsoDOWNRIGHT
+                                      // channel & bit24 (0x800000)-> alsoDOWN
+                                      // channel & bit25 (0x1000000)-> alsoDOWNLEFT
+                                      // channel & bit26 (0x2000000)-> alsoLEFT
   
   ClassDefNV(Cluster, 1);
 };
--- Detectors/TOF/reconstruction/src/Clusterer.cxx
+++ Detectors/TOF/reconstruction/src/Clusterer.cxx
@@ -33,12 +33,149 @@ void Clusterer::process(DataReader& reader, std::vector<Cluster>& clusters)
   while (reader.getNextStripData(mStripData)) {
     LOG(DEBUG) << "TOFClusterer got Strip " << mStripData.stripID << " Ndigits "
                << mStripData.digits.size() << FairLogger::endl;
+    
     initStrip();
-    for (int idig = 1; idig < mStripData.digits.size(); idig++)
+    /*    for (int idig = 1; idig < mStripData.digits.size(); idig++)
       updateStrip(idig);
-    finishStrip(clusters);
+    */
+    processStrip(clusters);
   }
 }
+
+//__________________________________________________
+void Clusterer::processStrip(std::vector<Cluster>& clusters)
+{
+  // method to clusterize the current strip
+
+  Int_t detId[5];
+  Int_t chan1, chan2, chan3;
+  Int_t strip1,strip2;
+  Int_t iphi,iphi2,iphi3;
+  Int_t ieta,ieta2,ieta3; // it is the number of padz-row increasing along the various strips
+  
+  for (int idig = 1; idig < mStripData.digits.size()-1; idig++) {
+    Digit* dig = &mStripData.digits[idig];
+    if (dig->isUsedInCluster()) continue; // the digit was already used to build a cluster
+    chan1 = dig->getChannel(); // note that inside the strip the digits are ordered per channel number
+    Geo::getVolumeIndices(chan1, detId); // Get volume index from channel index
+    ieta = detId[2]/*strip*/*2 + detId[3]/*pad Z*/;
+    if(detId[1]/*module*/ == 0) ieta += 0;
+    else if(detId[1] == 1) ieta += 38;
+    else if(detId[1] == 2) ieta += 76;
+    else if(detId[1] == 3) ieta += 106;
+    else if(detId[1] == 4) ieta += 144;
+    iphi = detId[0]/*phi sector*/*48 + detId[4]/*pad x*/;
+
+    // first we make a cluster out of the digit
+    clusters.emplace_back();
+    Cluster& c = clusters[noc];
+    c.setTime(dig->getTDC()*Geo::TDCBIN); // time in ps (for now we assume it calibrated)
+    c.setTot(dig->getTOT()*Geo::TOTBIN*1E-3); // TOT in ns (for now we assume it calibrated)
+    //c.setL0L1Latency(); // to be filled (maybe)
+    //c.setDeltaBC(); // to be filled (maybe)
+    c.setMainContributingChannel(chan1);
+    
+    for (int idigNext = idig+1; idigNext < mStripData.digits.size(); idigNext++) {
+      Digit* digNext = &mStripData.digits[idigNext];
+      if (digNext->isUsedInCluster()) continue; // the digit was already used to build a cluster
+      chan2 = digNext->getChannel(); // note that inside the strip the digits are ordered per channel number
+      if (chan1 == chan2) {
+	// the digits belong to the same channel, but were not merged; let's assume that they can make a cluster
+	LOG(DEBUG) << " found two digits from the same channel that were not merged - they can still make a cluster " << FairLogger::endl;
+      }
+      Geo::getVolumeIndices(chan2, detId); // Get volume index from channel index
+      ieta2 = detId[2]/*strip*/*2 + detId[3]/*pad Z*/;
+      if(detId[1]/*module*/ == 0) ieta2 += 0;
+      else if(detId[1] == 1) ieta2 += 38;
+      else if(detId[1] == 2) ieta2 += 76;
+      else if(detId[1] == 3) ieta2 += 106;
+      else if(detId[1] == 4) ieta2 += 144;
+      iphi2 = detId[0]/*phi sector*/*48 + detId[4]/*pad x*/;
+
+      // if(ieta3-ieta > 2) j = fN; // because cluster are order along Z so you can skip all the rest, go to the next one ("i+1")
+
+      // check if the fired pad are close in space
+      if((TMath::Abs(iphi-iphi2)>1) || (TMath::Abs(ieta-ieta2)>1)) 
+	continue;
+      
+      // check if the TOF time are close enough to be merged
+      float timeDig =; c.GetTime();
+      float timeDigNext = digNext->GetTDC()*Geo::TDCBIN; // we assume it calibrated (for now); in ps
+      
+      if(TMath::Abs(timeDig - timeDigNext) > 500/*in ps*/) continue;
+
+      int deltaPhi = iphi - iphi2; // difference in x between cluster and digit to be merged
+      int deltaEta = ieta - ieta2; // difference in z between cluster and digit to be merged
+      // merge the digit to the existing cluster
+      bool swapped = mergeClusters(c, digNext, deltaPhi, deltaEta);
+    } // loop on the second digit
+  } // loop on the first digit
+}
+
+//__________________________________________________
+bool Cluster::mergeClusters(Cluster& c, Digit* dig, int deltaPhi, int deltaEta){
+
+  // routine to merge two clusters
+
+  float totDigit = dig->getTOT()*Geo::TOTBIN*1E-3;
+  int mask;
+  if (deltaPhi == 1) { // the digit is to the LEFT of the cluster; let's check about UP/DOWN/Same Line
+    if (deltaEta == 1) { // the digit is DOWN LEFT wrt the cluster
+      mask = kDownLeft;
+    }
+    else if (deltaEta == -1) { // the digit is UP LEFT wrt the cluster
+      mask = kUpLeft;
+    }
+    else { // the digit is LEFT wrt the cluster
+      mask = kLeft;
+    }
+  }
+  else if (deltaPhi == -1) { // the digit is to the RIGHT of the cluster; let's check about UP/DOWN/Same Line
+    if (deltaEta == 1) { // the digit is DOWN RIGHT wrt the cluster
+      mask = kDownRight;
+    }
+    else if (deltaEta == -1) { // the digit is UP RIGHT wrt the cluster
+      mask = kUpRight;
+    }
+    else { // the digit is RIGHT wrt the cluster
+      mask = kRight;
+    }
+  }
+  else if (deltaPhi == 0) { // the digit is on the same column as the cluster; is it UP or Down?
+    if (deltaEta == 1) { // the digit is DOWN wrt the cluster
+      mask = kDown;
+    }
+    else if (deltaEta == -1) { // the digit is UP wrt the cluster
+      mask = kUp;
+    }
+    else { // impossible!!
+      LOG(DEBUG) << " Check what is going on, the digit you are trying to merge to the cluster must be in a different channels... " << FairLogger::endl;
+    }
+  }
+  else { // impossible!!! We checked above... 
+      LOG(DEBUG) << " Check what is going on, the digit you are trying to merge to the cluster is too far from the cluster, you should have not got here... " << FairLogger::endl;
+  }
+      
+  dig->setIsUsedInCluster();
+
+  if (c->GetTot() < totDigit) {
+      // the main channel contributing to the cluster is the one from the digit --> changing it
+    return kTRUE;
+  }
+  else {
+    c.addBitInContributingChannels(mask);
+  }
+
+  return kFALSE;
+
+}
+  
+  
+  
+
+
+
+      
 
 //__________________________________________________
 void Clusterer::initStrip()
@@ -65,7 +202,26 @@ void Clusterer::updateStrip(int idig)
 {
   Digit* dig = &mStripData.digits[idig];
 
-  // up to here for now... 
+  // up to here for now...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
   if (mCol != pix->col) { // switch the buffers
     Int_t* tmp = mCurr;
