c3d67538858db8441efddaff88ca95b79bee100e
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2020-11-19 15:50:30 +0100

    Intermediate commit
    remove dcs-proxy from Framework folder
    
    forgotten CMakeLists

--- Detectors/DCS/testWorkflow/dcs-proxy.cxx
+++ Detectors/DCS/testWorkflow/dcs-proxy.cxx
@@ -8,7 +8,8 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-// example to run: o2-dpl-dcs-proxy --dcs-proxy '--channel-config "name=dcs-proxy,type=pull,method=connect,address=tcp://10.11.28.22:60000,rateLogging=1,transport=zeromq"' -b
+// example to run:
+// o2-dpl-dcs-proxy --dcs-proxy '--channel-config "name=dcs-proxy,type=pull,method=connect,address=tcp://10.11.28.22:60000,rateLogging=1,transport=zeromq"' -b
 
 #include "Framework/WorkflowSpec.h"
 #include "Framework/DataProcessorSpec.h"
@@ -18,9 +19,16 @@
 #include "Framework/Lifetime.h"
 #include "Framework/ConfigParamSpec.h"
 #include "Framework/ExternalFairMQDeviceProxy.h"
+#include "DetectorsDCS/DataPointIdentifier.h"
+#include "DetectorsDCS/DataPointValue.h"
+#include "DetectorsDCS/DeliveryType.h"
 #include <vector>
+#include <unordered_map>
 
 using namespace o2::framework;
+using DPID = o2::dcs::DataPointIdentifier;
+using DPVAL = o2::dcs::DataPointValue;
+using DeliveryType = o2::dcs::DeliveryType;
 
 // we need to add workflow options before including Framework/runDataProcessing
 void customize(std::vector<ConfigParamSpec>& workflowOptions)
@@ -39,13 +47,38 @@ void customize(std::vector<ConfigParamSpec>& workflowOptions)
 
 WorkflowSpec defineDataProcessing(ConfigContext const& config)
 {
+
+  std::unordered_map<DPID, OutputSpec> dp2OutputSpec;
+  OutputSpec osTestDouble(ConcreteDataTypeMatcher{"DCS", "TESTDOUBLES"});
+  OutputSpec osTestBool(ConcreteDataTypeMatcher{"DCS", "TESTBOOLS"});
+
+  // this should be filled e.g. reading from CCDB. now for tests I hardcode it
+  DPID dpidtmp;
+  std::string dpAlias = "ADAPOS_LG/TEST_000260";
+  DPID::FILL(dpidtmp, dpAlias, DeliveryType::RAW_DOUBLE);
+  dp2OutputSpec[dpidtmp] = &osTestDouble;
+
+  dpAlias = "ADAPOS_LG/TEST_000261";
+  DPID::FILL(dpidtmp, dpAlias, DeliveryType::RAW_DOUBLE);
+  dp2OutputSpec[dpidtmp] = osTestDouble;
+
+  dpAlias = "ADAPOS_LG/TEST_000274";
+  DPID::FILL(dpidtmp, dpAlias, DeliveryType::RAW_BOOL);
+  dp2OutputSpec[dpidtmp] = osTestBool;
+
+  dpAlias = "ADAPOS_LG/TEST_000275";
+  DPID::FILL(dpidtmp, dpAlias, DeliveryType::RAW_BOOL);
+  dp2OutputSpec[dpidtmp] = osTestBool;
+ 
   Outputs dcsOutput;
-  dcsOutput.emplace_back("DCS", "DATAPOINTS", 0, Lifetime::Timeframe);
+  //dcsOutput.emplace_back(ConcreteDataTypeMatcher{"DCS", "DATAPOINTS"});
+  dcsOutput.push_back(osTestDouble);
+  dcsOutput.push_back(osTestBool);
   
   DataProcessorSpec dcsProxy = specifyExternalFairMQDeviceProxy(
     "dcs-proxy",
     std::move(dcsOutput),
-    "type=pull,method=connect,address=tcp://aldcsadaposactor:60000,rateLogging=1,transport=zmq",
+    "type=pull,method=connect,address=tcp://aldcsadaposactor:60000,rateLogging=1,transport=zeromq",
     incrementalConverter(dcsOutput[0], 0, 1));
 
   WorkflowSpec workflow;
