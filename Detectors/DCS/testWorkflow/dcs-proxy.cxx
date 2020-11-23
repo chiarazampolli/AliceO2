d94a552ad8641e8228cea65857ab8bdc49e7367f
shahoian <ruben.shahoyan@cern.ch>
2020-11-23 16:52:54 +0100

    add verbosity flag to dcs-proxy + misc fixes

--- Detectors/DCS/testWorkflow/dcs-proxy.cxx
+++ Detectors/DCS/testWorkflow/dcs-proxy.cxx
@@ -9,7 +9,7 @@
 // or submit itself to any jurisdiction.
 
 // example to run:
-// o2-dcs-dcs-proxy --dcs-proxy '--channel-config "name=dcs-proxy,type=pull,method=connect,address=tcp://10.11.28.22:60000,rateLogging=1,transport=zeromq"' -b
+// o2-dcs-proxy --dcs-proxy '--channel-config "name=dcs-proxy,type=pull,method=connect,address=tcp://10.11.28.22:60000,rateLogging=1,transport=zeromq"' -b
 
 #include "Framework/WorkflowSpec.h"
 #include "Framework/DataProcessorSpec.h"
@@ -34,10 +34,9 @@ using DeliveryType = o2::dcs::DeliveryType;
 // we need to add workflow options before including Framework/runDataProcessing
 void customize(std::vector<ConfigParamSpec>& workflowOptions)
 {
-  /*  workflowOptions.push_back(
-    ConfigParamSpec{
-      "dataspec", VariantType::String, "A:FLP/RAWDATA;B:FLP/DISTSUBTIMEFRAME/0", {"selection string for the data to be proxied"}});
-
+  workflowOptions.push_back(
+    ConfigParamSpec{"verbose", VariantType::Bool, false, {"verbose output"}});
+  /*
   workflowOptions.push_back(
     ConfigParamSpec{
       "throwOnUnmatched", VariantType::Bool, false, {"throw if unmatched input data is found"}});
@@ -72,13 +71,18 @@ WorkflowSpec defineDataProcessing(ConfigContext const& config)
   DPID::FILL(dpidtmp, dpAlias, DeliveryType::RAW_BOOL);
   dp2OutputSpec[dpidtmp] = osTestBool;
   */
-
+  bool verbose = config.options().get<bool>("verbose");
   DPID dpidtmp;
 
   std::unordered_map<DPID, o2h::DataDescription> dpid2DataDesc;
-  std::string dpAlias = "ADAPOS_LG/TEST_000261";
-  DPID::FILL(dpidtmp, dpAlias, DeliveryType::RAW_DOUBLE);
+  DPID::FILL(dpidtmp, "ADAPOS_LG/TEST_000100", DeliveryType::RAW_STRING);
   dpid2DataDesc[dpidtmp] = "COMMON"; // i.e. this will go to {DCS/COMMON/0} OutputSpec
+  DPID::FILL(dpidtmp, "ADAPOS_LG/TEST_000110", DeliveryType::RAW_STRING);
+  dpid2DataDesc[dpidtmp] = "COMMON";
+  DPID::FILL(dpidtmp, "ADAPOS_LG/TEST_000200", DeliveryType::RAW_STRING);
+  dpid2DataDesc[dpidtmp] = "COMMON1";
+  DPID::FILL(dpidtmp, "ADAPOS_LG/TEST_000240", DeliveryType::RAW_INT);
+  dpid2DataDesc[dpidtmp] = "COMMON1";
 
   // RS: here we should complete the attribution of different DPs to different outputs
   // ...
@@ -98,7 +102,7 @@ WorkflowSpec defineDataProcessing(ConfigContext const& config)
     "dcs-proxy",
     std::move(dcsOutputs),
     "type=pull,method=connect,address=tcp://aldcsadaposactor:60000,rateLogging=1,transport=zeromq",
-    dcs2dpl(dpid2DataDesc, 0, 1));
+    dcs2dpl(dpid2DataDesc, 0, 1, verbose));
 
   WorkflowSpec workflow;
   workflow.emplace_back(dcsProxy);
