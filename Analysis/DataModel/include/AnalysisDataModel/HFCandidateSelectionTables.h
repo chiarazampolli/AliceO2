d754e804331327286da906941e3b586b89696c4e
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2021-02-23 00:38:28 +0100

    Comments by Vit
    clang
    
    Fixes for braces
    
    clang

--- Analysis/DataModel/include/AnalysisDataModel/HFCandidateSelectionTables.h
+++ Analysis/DataModel/include/AnalysisDataModel/HFCandidateSelectionTables.h
@@ -41,9 +41,9 @@ DECLARE_SOA_TABLE(HFSelJpsiToEECandidate, "AOD", "HFSELJPSICAND", hf_selcandidat
 
 namespace hf_selcandidate_lc_k0sp
 {
-DECLARE_SOA_COLUMN(IsSelLcK0sp, isSelLcK0sp, int);
+DECLARE_SOA_COLUMN(IsSelLcK0sP, isSelLcK0sP, int);
 } // namespace hf_selcandidate_lc_k0sp
-DECLARE_SOA_TABLE(HFSelLcK0spCandidate, "AOD", "HFSELLCK0SPCAND", hf_selcandidate_lc_k0sp::IsSelLcK0sp);
+DECLARE_SOA_TABLE(HFSelLcK0sPCandidate, "AOD", "HFSELLCK0SPCAND", hf_selcandidate_lc_k0sp::IsSelLcK0sP);
 
 } // namespace o2::aod
 #endif // O2_ANALYSIS_HFCANDIDATESELECTIONTABLES_H_
